c6bdf8405b817df8afff6faf5f906a101173e64d
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-04 23:34:45 -0300

    Criado o arquivo EntradaDeDados

--- Avulsos/MediaSimples.dart
+++ Avulsos/MediaSimples.dart
@@ -1,17 +1,19 @@
 void main() {
+  int nota1 = 6;
+  int nota2 = 7;
+  int nota3 = 3;
 
-    var nota1 = 6;
-    var nota2 = 7;
-    var nota3 = 8;
-    
-    var media = (nota1 + nota2 + nota3) / 3;
+  double media = (nota1 + nota2 + nota3) / 3;
 
-    if (media >= 7) {
-      print("Aprovado");
-    } else if((media >= 5) && (media < 7)) {
-      print("Recuperação");
-    }else {
-      print("Reprovado");
-    }
-       
+  // if (media >= 7) {
+  //   print("Aprovado");
+  // } else if ((media >= 5) && (media < 7)) {
+  //   print("Recuperação");
+  // } else {
+  //   print("Reprovado");
+  // }
+
+  var resultado = (media >= 7) ? "Aprovado" : "Reprovado";
+
+  print(resultado);
 }
