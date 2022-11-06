356e66fe0da18e265fdba1e7a70cb112b93de6fa
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-06 11:13:45 -0300

    Laço Do...While

---
+++ Avulsos/LacoDoWhile.dart
@@ -0,0 +1,10 @@
+void main() {
+  int valor = 0;
+  do {
+    // Valores pares até o numero que valor assumir
+    if (valor % 2 == 0) {
+      print(valor);
+    }
+    valor++;
+  } while (valor < 10);
+}
