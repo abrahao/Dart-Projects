0e6b30bc7629cdb86b89a3eaf9dc4d6ffdd87ed7
Abrahao Eneias <abrahao.eneias@dce.ufpb.br>
2022-11-07 20:47:29 -0300

    Criacao do projeto meu_app_oo

---
+++ Avulsos/meu_app_oo/bin/meu_app_oo.dart
@@ -0,0 +1,5 @@
+import 'package:meu_app_oo/meu_app_oo.dart' as meu_app_oo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meu_app_oo.calculate()}!');
+}
---
+++ Avulsos/meu_app_oo/test/meu_app_oo_test.dart
@@ -0,0 +1,8 @@
+import 'package:meu_app_oo/meu_app_oo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
